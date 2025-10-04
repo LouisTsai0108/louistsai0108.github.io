f931396d43341a8d9b80ef07de997e74b75dccec
LouisTsai0108 <95480274+LouisTsai0108@users.noreply.github.com>
2025-10-04 16:02:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -160,8 +160,9 @@ let index = 0;
 
 function showSlide(i) {
   index = (i + images.length) % images.length;
-  slides.style.transform = `translateX(${-index * 400}px)`; 
+  slides.style.transform = `translateX(-${index * 100}%)`; 
 }
 
 prev.addEventListener("click", () => showSlide(index - 1));
 next.addEventListener("click", () => showSlide(index + 1));
+
